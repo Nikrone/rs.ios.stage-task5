e9d8a29701a1a2e909856a1082b1b2eb95f60edc
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-06-14 16:37:55 +0300

    feat: add implemented Exercise 1, 2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,39 @@
 import Foundation
 
 class StockMaximize {
-
+    
+    func getCount(prices: [Int]) -> Int {
+        var profit = 0
+        for price in prices {
+            profit += prices .last! - price
+        }
+        return profit
+    }
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        guard prices.count > 1 else {
+            return 0
+        }
+        
+        var profit = 0
+        var somePrice = [Int]()
+        somePrice.append(prices.first!)
+        
+        for index in 0...(prices.count-2) {
+            if prices [index] <= prices [index+1] {
+                somePrice.append(prices[index+1])
+            } else {
+                if somePrice.count > 1 {
+                    profit += getCount(prices: somePrice)
+                }
+                somePrice.removeAll()
+                somePrice.append(prices[index+1])
+            }
+        }
+        
+        if somePrice.count > 1 {
+            profit += getCount(prices: somePrice)
+        }
+        return profit
     }
 }
